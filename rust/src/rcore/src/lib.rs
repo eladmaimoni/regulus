f59f9c2ff8db1d5d5ef01e43f9269c2f8454dfed
Elad Maimoni <elmaimon@cisco.com>
2026-03-16 15:28:36 +0200

    Re-export tracing functions at crate root and define CXX bridge for Rust functions

--- rust/src/rcore/src/lib.rs
+++ rust/src/rcore/src/lib.rs
@@ -1,8 +1,27 @@
 pub mod tracing_facade;
 
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+
+// CXX needs those functions to be visible at the crate root scope (not behind tracing_facade::). 
+// We need to re-export them
+pub use tracing_facade::{initialize_tracing, trace_info};
+
+#[cxx::bridge]
+mod ffi {
+    extern "Rust" {
+        fn initialize_tracing();
+        fn trace_info(event: &str);
+    }
+}
+
+
+
+
+
 
 #[cfg(test)]
 mod tests {
