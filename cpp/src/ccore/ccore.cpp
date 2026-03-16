d123af3c02e5c9c9bf331f954459ae95999930a3
Elad Maimoni <elmaimon@cisco.com>
2026-03-16 18:14:46 +0200

    project compiles fine with md, but fails linkage

--- cpp/src/ccore/ccore.cpp
+++ cpp/src/ccore/ccore.cpp
@@ -1,9 +1,12 @@
 #include "ccore/ccore.hpp"
+#include "rcore_cxx/lib.h"
 
 namespace rg
 {
     int add_cpp(int a, int b)
     {
+        initialize_tracing();
+        trace_info("hi from cpp");
         return a + b;
     }
 }
