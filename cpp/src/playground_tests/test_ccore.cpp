2472fe37e1e25b6e205a9679d64e2a4c0f9e5375
Elad Maimoni <elmaimon@cisco.com>
2026-03-16 17:11:05 +0200

    Update smoke test in test_ccore.cpp to validate addition functionality using rg::add_cpp

--- cpp/src/playground_tests/test_ccore.cpp
+++ cpp/src/playground_tests/test_ccore.cpp
@@ -1,6 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
+#include <ccore/ccore.hpp>
 
 TEST_CASE("smoke test", "[ccore][smoke]") {
-    REQUIRE(1 + 1 == 2);
+    auto result = rg::add_cpp(1, 2);
+    REQUIRE(result == 3);
 }
