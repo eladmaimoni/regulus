217cc5efbebce6229940b75cf48f2d8ea57c963e
Elad Maimoni <elmaimon@cisco.com>
2026-03-17 19:56:30 +0200

    Add options for rcore and tests in CMake configuration
    - Introduced REGULUS_WITH_RCORE and REGULUS_WITH_TESTS options to enable building with rcore and running tests.
    - Created a new import_rcore_target.cmake file to handle rcore integration.
    - Updated src/CMakeLists.txt to conditionally add playground_tests based on the REGULUS_WITH_TESTS option.

---
+++ cpp/cmake/import_rcore_target.cmake
@@ -0,0 +1,49 @@
+function(import_rcore_target)
+    FetchContent_Declare(
+        Corrosion
+        GIT_REPOSITORY https://github.com/corrosion-rs/corrosion.git
+        GIT_TAG master
+    )
+    FetchContent_MakeAvailable(Corrosion)
+
+    corrosion_import_crate(MANIFEST_PATH ${CMAKE_CURRENT_SOURCE_DIR}/../rust/src/rcore/Cargo.toml)
+    if(NOT TARGET rcore)
+        message(FATAL_ERROR "rcore target not found")
+    endif()
+
+    if(WIN32)
+        # On Windows, we need to link to the bcrypt libraries
+        # to get the necessary functionality for the rcore crate.
+        target_link_libraries(rcore INTERFACE bcrypt)
+        # get_target_property(_rcore_type rcore TYPE)
+        # if(_rcore_type STREQUAL "STATIC_LIBRARY")
+        #     message(STATUS "rcore target is a static library")
+        # else()
+        #     message(STATUS "rcore target is not a static library: ${_rcore_type}")
+        # endif()
+    endif()
+
+    if(MSVC)
+        string(FIND "${CMAKE_MSVC_RUNTIME_LIBRARY}" "DLL" _is_dynamic_crt)
+        if(NOT _is_dynamic_crt EQUAL -1)
+            message(STATUS "we are using a dynamic CRT, making sure cxx internally (in its build.rs) links the debug version of the CRT")
+            corrosion_set_env_vars(rcore
+                "CFLAGS=$<IF:$<CONFIG:Debug>,/MDd,/MD>"
+                "CXXFLAGS=$<IF:$<CONFIG:Debug>,/MDd,/MD>"
+            )
+        else()
+            message(STATUS "we are using a static CRT, making sure cxx internally (in its build.rs) links the release version of the CRT")
+            corrosion_set_env_vars(rcore
+                "CFLAGS=$<IF:$<CONFIG:Debug>,/MTd,/MT>"
+                "CXXFLAGS=$<IF:$<CONFIG:Debug>,/MTd,/MT>"
+            )
+        endif()
+    endif()
+
+
+    corrosion_add_cxxbridge(rcore_cxx
+        CRATE rcore
+        FILES lib.rs
+    )
+    add_library(rcore::rcore ALIAS rcore)
+endfunction()
