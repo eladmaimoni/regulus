4b3b9137e903a416052f8f200cce4ad19331ffc1
Elad Maimoni <elmaimon@cisco.com>
2026-03-25 15:44:36 +0200

    fixed msvc build

--- cpp/cmake/import_rcore_target.cmake
+++ cpp/cmake/import_rcore_target.cmake
@@ -15,6 +15,8 @@ function(import_rcore_target)
         # On Windows, we need to link to the bcrypt libraries
         # to get the necessary functionality for the rcore crate.
         target_link_libraries(rcore INTERFACE bcrypt)
+
+
         # get_target_property(_rcore_type rcore TYPE)
         # if(_rcore_type STREQUAL "STATIC_LIBRARY")
         #     message(STATUS "rcore target is a static library")
@@ -28,14 +30,17 @@ function(import_rcore_target)
         if(NOT _is_dynamic_crt EQUAL -1)
             message(STATUS "we are using a dynamic CRT, making sure cxx internally (in its build.rs) links the debug version of the CRT")
             corrosion_set_env_vars(rcore
-                "CFLAGS=$<IF:$<CONFIG:Debug>,/MDd,/MD>"
-                "CXXFLAGS=$<IF:$<CONFIG:Debug>,/MDd,/MD>"
+                "CFLAGS_x86_64_pc_windows_msvc=$<IF:$<CONFIG:Debug>,/MDd,/MD>"
+                "CXXFLAGS_x86_64_pc_windows_msvc=$<IF:$<CONFIG:Debug>,/MDd,/MD>"
             )
         else()
+            target_link_options(rcore INTERFACE
+                $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<CONFIG:Debug>>:/NODEFAULTLIB:libcmt>
+            )
             message(STATUS "we are using a static CRT, making sure cxx internally (in its build.rs) links the release version of the CRT")
             corrosion_set_env_vars(rcore
-                "CFLAGS=$<IF:$<CONFIG:Debug>,/MTd,/MT>"
-                "CXXFLAGS=$<IF:$<CONFIG:Debug>,/MTd,/MT>"
+                "CFLAGS_x86_64_pc_windows_msvc=$<IF:$<CONFIG:Debug>,/MTd,/MT>"
+                "CXXFLAGS_x86_64_pc_windows_msvc=$<IF:$<CONFIG:Debug>,/MTd,/MT>"
             )
         endif()
     endif()
